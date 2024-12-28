9039a5817bcba867f729af2b6fbf226130b39eb4
Félix Gagnon <felgag13@gmail.com>
2024-12-28 07:57:51 -0500

    init

---
+++ lua/lazy-plugins.lua
@@ -0,0 +1,40 @@
+-- [[ Configure and install plugins ]]
+require('lazy').setup({
+  -- THEMES
+  require 'themes.tokyonight',
+  -- PLUGINS
+  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  require 'plugins.which-key',
+  require 'plugins.telescope',
+  require 'plugins.lspconfig',
+  require 'plugins.cmp',
+  require 'plugins.mini',
+  require 'plugins.treesitter',
+  require 'plugins.debug',
+  require 'plugins.indent_line',
+  require 'plugins.lint',
+  require 'plugins.autopairs',
+  require 'plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'plugins.conform',
+}, {
+  ui = {
+    -- If you are using a Nerd Font: set icons to an empty table which will use the
+    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      lazy = '💤 ',
+    },
+  },
+})
