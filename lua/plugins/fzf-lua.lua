899483ad6cb34c46f1e55a79a94db518b5ef09bf
Felix Gagnon <felgag13@gmail.com>
2025-08-17 12:08:38 -0400

    configure fzf-lua

---
+++ lua/plugins/fzf-lua.lua
@@ -0,0 +1,5 @@
+return {
+    "ibhagwan/fzf-lua",
+    event = "VeryLazy",
+    config = function() require "plugins.config.fzf-lua" end
+}
---
+++ lua/plugins/gruvbox.lua
@@ -0,0 +1,11 @@
+	return {
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000,
+		opts = {
+			contrast = "soft",
+			transparent_mode = false,
+		},
+		init = function()
+			vim.cmd("colorscheme gruvbox")
+		end,
+	};
